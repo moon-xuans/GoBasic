1a3779d14da9c4c2464ad076c726670c75229ecd
zhouxuan <moon_xuans@163.com>
2022-12-04 14:45:41 +0800

    类型转换，类型别名

---
+++ src/com/axuan/demo02/demo06.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 类型别名
+func main() {
+	// 给int64起一个别名叫bigint
+	type bigint int64
+
+	var a bigint // 等价于var a int64
+	fmt.Printf("a type is %T\n", a)
+
+	type (
+		long int64
+		char byte
+	)
+
+	var b long = 11
+	var ch char = 'a'
+	fmt.Printf("b = %d, ch = %c \n", b, ch)
+}
