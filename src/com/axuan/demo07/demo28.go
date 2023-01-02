1525ef29668becb72fcb94994744864561f6caae
zhouxuan <moon_xuans@163.com>
2023-01-02 14:53:12 +0800

    map

---
+++ src/com/axuan/demo07/demo28.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// map做函数参数
+
+func test(m map[int]string) {
+	delete(m, 1)
+}
+
+func main() {
+	m := map[int]string{1: "mike", 2: "yoyo", 3: "go"}
+	fmt.Println("m = ", m)
+
+	test(m) // 在函数内部删除某个key
+
+	fmt.Println("m = ", m)
+}
