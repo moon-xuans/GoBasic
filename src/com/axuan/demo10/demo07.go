ecaae0a4b2a5aff49ba6272cd2bdbe9d85f02e40
zhouxuan <moon_xuans@163.com>
2023-01-25 22:11:06 +0800

    json

---
+++ src/com/axuan/demo10/demo07.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// 通过map生成json
+
+func main() {
+	// 创建一个map
+	m := make(map[string]interface{}, 4)
+	m["company"] = "itcast"
+	m["subjects"] = []string{"Go", "C++", "Python", "Test"}
+	m["isok"] = true
+	m["price"] = 666.666
+
+	// 编码成json
+	//result, err := json.Marshal(m)
+	result, err := json.MarshalIndent(m, "", " ")
+
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+	fmt.Println("result = ", string(result))
+}
