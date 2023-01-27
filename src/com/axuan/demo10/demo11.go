effae8b986f085fa37d4a22b252e513c5ee29cee
zhouxuan <moon_xuans@163.com>
2023-01-27 21:47:00 +0800

    文件

--- src/com/axuan/demo10/demo11.go
+++ src/com/axuan/demo10/demo11.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -32,6 +34,59 @@ func WriteFile(path string) {
 	}
 }
 
+func ReadFile(path string) {
+	// 打开文件
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+
+	// 关闭文件
+	defer f.Close()
+
+	buf := make([]byte, 1024*2) // 2k大小
+
+	// n代表从文件读取内容的长度
+	n, err1 := f.Read(buf)
+	if err1 != nil && err1 != io.EOF {
+		fmt.Println("err1 = ", err1)
+		return
+	}
+	fmt.Println("buf = ", string(buf[:n]))
+}
+
+func ReadFileLine(path string) {
+	// 打开文件
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+
+	// 关闭文件
+	defer f.Close()
+
+	// 新建一个缓冲区，把内容先放在缓冲区
+	r := bufio.NewReader(f)
+
+	for {
+		// 遇到'\n'结束读取，但是'\n'也读取进入
+		buf, err := r.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF { // 文件已经结束
+				break
+			}
+			fmt.Println("err = ", err)
+		}
+
+		fmt.Printf("buf = #%s#\n", string(buf))
+	}
+}
+
 func main() {
-	WriteFile("./demo.txt")
+	path := "./demo.txt"
+	//WriteFile(path)
+	//ReadFile(path)
+	ReadFileLine(path)
 }
